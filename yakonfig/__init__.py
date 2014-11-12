e116636ec26ad3201dad06f83193acc03d0ce02c
David Maze <dmaze@diffeo.com>
2014-11-12 15:12:28 -0500

    For Sphinx, split CLI vs. Python API docs.
    'sphinx -t cliref' only outputs the section on common command-line arguments.
    'sphinx -t pyapi' only outputs the Python API.
    'sphinx -t all' does everything.

--- yakonfig/__init__.py
+++ yakonfig/__init__.py
@@ -3,144 +3,144 @@
 .. This software is released under an MIT/X11 open source license.
    Copyright 2013-2014 Diffeo, Inc.
 
-Loads a YAML file (with extensions) and makes it available globally to
-a Python application.  Application code can call
-:func:`yakonfig.get_global_config` to get values from the
-configuration store.
+.. only:: pyapi or all
 
-In typical use, a program will declare some number of top-level
-configuration modules.  These modules mimic the structure of
-:class:`yakonfig.Configurable`, and are passed in directly to
-:func:`yakonfig.parse_args()`.  This makes the corresponding
-configuration available in the global configuration.  For example::
+    Loads a YAML file (with extensions) and makes it available
+    globally to a Python application.  Application code can call
+    :func:`yakonfig.get_global_config` to get values from the
+    configuration store.
 
-    # I am a_module.py
-    import yakonfig
+    In typical use, a program will declare some number of top-level
+    configuration modules.  These modules mimic the structure of
+    :class:`yakonfig.Configurable`, and are passed in directly to
+    :func:`yakonfig.parse_args()`.  This makes the corresponding
+    configuration available in the global configuration.  For
+    example::
 
-    # Configurable metadata
-    config_name = 'a_module'
-    default_config = { 'message': 'hello world' }
-    def add_arguments(parser):
-        parser.add_argument('--message')
-    runtime_keys = { 'message': 'message' }
+        # I am a_module.py
+        import yakonfig
 
-    def main():
-        parser = argparse.ArgumentParser()
-        args = yakonfig.parse_args(parser, [yakonfig, a_module])
-        print yakonfig.get_global_config('a_module', 'message')
-    if __name__ == '__main__':
-        main()
+        # Configurable metadata
+        config_name = 'a_module'
+        default_config = { 'message': 'hello world' }
+        def add_arguments(parser):
+            parser.add_argument('--message')
+        runtime_keys = { 'message': 'message' }
 
-Running ``python -m a_module`` will print out "hello world"; running
-``python -m a_module --message goodbye`` will inject the "goodbye"
-message into the configuration, and then print it out.
+        def main():
+            parser = argparse.ArgumentParser()
+            args = yakonfig.parse_args(parser, [yakonfig, a_module])
+            print yakonfig.get_global_config('a_module', 'message')
+        if __name__ == '__main__':
+            main()
 
-.. note:: Avoid using :meth:`argparse.ArgumentParser.set_defaults` in
-          combination with :func:`yakonfig.parse_args`.  Any default
-          values set this way will override values set in the user's
-          configuration file.
+    Running ``python -m a_module`` will print out "hello world";
+    running ``python -m a_module --message goodbye`` will inject the
+    "goodbye" message into the configuration, and then print it out.
 
-Most of the useful functions and objects in the submodules are
-re-exported from the top-level module.
+    .. note:: Avoid using :meth:`argparse.ArgumentParser.set_defaults`
+              in combination with :func:`yakonfig.parse_args`.  Any
+              default values set this way will override values set in
+              the user's configuration file.
 
-Common command-line arguments
-=============================
+    Most of the useful functions and objects in the submodules are
+    re-exported from the top-level module.
 
-All programs based on :mod:`yakonfig` support the following
-command-line arguments.
+.. only:: all
 
-.. program:: yakonfig
+    Common command-line arguments
+    =============================
 
-.. option:: --config <file.yaml>, -c <file.yaml>
+    All programs based on :mod:`yakonfig` support the following
+    command-line arguments.
 
-Load ``file.yaml`` as the primary configuration file.  Options set in
-the configuration file override any default values; options set on the
-command line override values in this configuration file.
+.. only:: cliref or all
 
-.. option:: --dump-config {default|effective|full}
+    .. program:: yakonfig
 
-Write the configuration as YAML to standard output, then stop
-immediately.  The argument indicates which configuration is dumped: if
-``default`` then the default configuration, ignoring all user setup,
-is dumped; if ``full`` then the dump contains the complete
-configuration, including all default settings, settings from the
-configuration file, and command-line options; and if ``effective``
-then the dump contains only those settings that differ from the
-defaults, producing the minimum configuration to recreate the current
-settings.
+    .. option:: --config <file.yaml>, -c <file.yaml>
 
-YAML extensions
-===============
+        Load `file.yaml` as the primary configuration file.  Options
+        set in the configuration file override any default values;
+        options set on the command line override values in this
+        configuration file.
 
-``key: !include path``
-  Loads a yaml file at `path` and inserts it as the value associated
-  with `key`.
+    .. option:: --dump-config {default|effective|full}
 
-``key: !include_yaml path`` *(deprecated)*
-  Alias for ``!include``.
+        Write the configuration as YAML to standard output, then stop
+        immediately.  The argument indicates which configuration is
+        dumped: if ``default`` then the default configuration,
+        ignoring all user setup, is dumped; if ``full`` then the dump
+        contains the complete configuration, including all default
+        settings, settings from the configuration file, and
+        command-line options; and if ``effective`` then the dump
+        contains only those settings that differ from the defaults,
+        producing the minimum configuration to recreate the current
+        settings.
 
-``key: !include_func package.path.to.func`` *(deprecated)*
-  Calls a python function. from a fully specified name of
-  ``package.func`` If the function name ends in "yaml" the return
-  value is interpreted as a yaml document body in a string and
-  parsed. Otherwise the return value is assumed to be a dict or other
-  object that can be simply assigned to they key at this point in the
-  yaml file.
+.. only:: pyapi or all
 
-Top-level entry points
-======================
+    YAML extensions
+    ===============
 
-.. autofunction:: parse_args
-.. autofunction:: set_default_config
-.. autofunction:: defaulted_config
-.. autofunction:: get_global_config
-.. autofunction:: set_global_config
-.. autofunction:: clear_global_config
+    ``key: !include path``
+      Loads a yaml file at `path` and inserts it as the value
+      associated with `key`.
 
-Configurable modules
-====================
+    Top-level entry points
+    ======================
 
-Modules that can be configured by the user implement the
-:class:`yakonfig.Configurable` interface, or more often, have their
-classes and modules provide the same names.  Anything that includes a
-:attr:`~yakonfig.Configurable.config_name` property can be passed into
-the top-level entry points above.
+    .. autofunction:: parse_args
+    .. autofunction:: set_default_config
+    .. autofunction:: defaulted_config
+    .. autofunction:: get_global_config
+    .. autofunction:: set_global_config
+    .. autofunction:: clear_global_config
 
-.. autoclass:: Configurable
-   :members:
+    Configurable modules
+    ====================
 
-.. autoclass:: ProxyConfigurable
-   :members:
+    Modules that can be configured by the user implement the
+    :class:`yakonfig.Configurable` interface, or more often, have their
+    classes and modules provide the same names.  Anything that includes a
+    :attr:`~yakonfig.Configurable.config_name` property can be passed into
+    the top-level entry points above.
 
-.. autoclass:: NewSubModules
-   :members:
+    .. autoclass:: Configurable
+       :members:
 
-.. autofunction:: check_toplevel_config
-.. autofunction:: check_subconfig
+    .. autoclass:: ProxyConfigurable
+       :members:
 
-Operations on configuration
-===========================
+    .. autoclass:: NewSubModules
+       :members:
 
-Configuration is always passed around as plain Python dictionaries.
-If you can guarantee that a configuration has passed through yakonfig,
-then you can generally guarantee that its default values are present
-and that any defined checks have passed.
+    .. autofunction:: check_toplevel_config
+    .. autofunction:: check_subconfig
 
-.. autofunction:: overlay_config
-.. autofunction:: diff_config
+    Operations on configuration
+    ===========================
 
-Exceptions
-==========
+    Configuration is always passed around as plain Python dictionaries.
+    If you can guarantee that a configuration has passed through yakonfig,
+    then you can generally guarantee that its default values are present
+    and that any defined checks have passed.
 
-.. autoclass:: ConfigurationError
-.. autoclass:: ProgrammerError
+    .. autofunction:: overlay_config
+    .. autofunction:: diff_config
 
-Interactive programs
-====================
+    Exceptions
+    ==========
 
-.. currentmodule:: yakonfig.cmd
-.. autoclass:: ArgParseCmd
-.. currentmodule:: yakonfig
+    .. autoclass:: ConfigurationError
+    .. autoclass:: ProgrammerError
+
+    Interactive programs
+    ====================
+
+    .. currentmodule:: yakonfig.cmd
+    .. autoclass:: ArgParseCmd
+    .. currentmodule:: yakonfig
 
 '''
 from __future__ import absolute_import
